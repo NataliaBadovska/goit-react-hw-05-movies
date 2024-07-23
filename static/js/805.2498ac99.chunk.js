e16862addb0023180cb61b4fff7e4da8cc49d6cd
NataliaBadovska <106466046+NataliaBadovska@users.noreply.github.com>
2024-07-23 11:38:45 +0000

    Deploying to gh-pages from @ NataliaBadovska/goit-react-hw-05-movies@0852db060134450b0cb2920a4d2e0ead50d92ca3 🚀

---
+++ static/js/805.2498ac99.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,r){r.r(t);var c=r(87),n=r(184);t.default=function(){return(0,n.jsxs)("h2",{children:["\u0417\u0430 \u0434\u0430\u043d\u0438\u043c URL \u043d\u0456\u0447\u043e\u0433\u043e \u043d\u0435 \u0437\u043d\u0430\u0439\u0434\u0435\u043d\u043e. \u0413\u043e\u043b\u043e\u0432\u043d\u0430 \u0441\u0442\u043e\u0440\u0456\u043d\u043a\u0430 ",(0,n.jsx)(c.rU,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=805.2498ac99.chunk.js.map
